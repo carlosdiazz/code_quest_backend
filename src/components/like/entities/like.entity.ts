1e921b0c8689ae8dba8743b62a3a84faa8886a29
Carlos <c.diazadriann@gmail.com>
2025-09-12 14:18:33 -0400

    Agregando Like

--- src/components/like/entities/like.entity.ts
+++ src/components/like/entities/like.entity.ts
@@ -1,7 +1,31 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
+import {
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
+@Entity({ name: 'like' })
 @ObjectType()
 export class Like {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  public id: number;
+
+  @Field(() => Date)
+  @CreateDateColumn({
+    name: 'create_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  public createAt: Date;
+
+  @Field(() => Date)
+  @UpdateDateColumn({
+    name: 'update_at',
+    type: 'timestamptz',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  public updateAt: Date;
 }
